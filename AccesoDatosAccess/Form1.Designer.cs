7e4a4bd0dc85230657918e55ef8d2b96b1ec4697
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2022-11-15 14:38:49 +0100

    Agregar archivos de proyecto.

---
+++ AccesoDatosAccess/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace AccesoDatosAccess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.btnExaminar = new System.Windows.Forms.Button();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtComentario = new System.Windows.Forms.TextBox();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.txtFechaAlta = new System.Windows.Forms.TextBox();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.Red;
+            this.Label1.Location = new System.Drawing.Point(12, 19);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(738, 44);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Ejemplo de acceso a datos con una base de datos de Access usando ADO.NET y OleDb." +
+    "";
+            // 
+            // btnExaminar
+            // 
+            this.btnExaminar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExaminar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnExaminar.Location = new System.Drawing.Point(608, 66);
+            this.btnExaminar.Name = "btnExaminar";
+            this.btnExaminar.Size = new System.Drawing.Size(75, 23);
+            this.btnExaminar.TabIndex = 6;
+            this.btnExaminar.Text = "Examinar...";
+            // 
+            // txtBase
+            // 
+            this.txtBase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBase.Location = new System.Drawing.Point(212, 66);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(390, 20);
+            this.txtBase.TabIndex = 5;
+            this.txtBase.Text = "txtBase";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnConectar.Location = new System.Drawing.Point(84, 66);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(124, 23);
+            this.btnConectar.TabIndex = 4;
+            this.btnConectar.Text = "Conectar a la base";
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GroupBox1.Controls.Add(this.txtComentario);
+            this.GroupBox1.Controls.Add(this.Label7);
+            this.GroupBox1.Controls.Add(this.btnNuevo);
+            this.GroupBox1.Controls.Add(this.btnActualizar);
+            this.GroupBox1.Controls.Add(this.txtFechaAlta);
+            this.GroupBox1.Controls.Add(this.Label6);
+            this.GroupBox1.Controls.Add(this.txtEmail);
+            this.GroupBox1.Controls.Add(this.Label5);
+            this.GroupBox1.Controls.Add(this.txtNombre);
+            this.GroupBox1.Controls.Add(this.Label4);
+            this.GroupBox1.Controls.Add(this.txtID);
+            this.GroupBox1.Controls.Add(this.Label3);
+            this.GroupBox1.Controls.Add(this.Label2);
+            this.GroupBox1.Controls.Add(this.btnLast);
+            this.GroupBox1.Controls.Add(this.btnNext);
+            this.GroupBox1.Controls.Add(this.btnPrev);
+            this.GroupBox1.Controls.Add(this.btnFirst);
+            this.GroupBox1.Location = new System.Drawing.Point(33, 114);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(650, 434);
+            this.GroupBox1.TabIndex = 7;
+            this.GroupBox1.TabStop = false;
+            // 
+            // txtComentario
+            // 
+            this.txtComentario.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtComentario.Location = new System.Drawing.Point(120, 164);
+            this.txtComentario.Multiline = true;
+            this.txtComentario.Name = "txtComentario";
+            this.txtComentario.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtComentario.Size = new System.Drawing.Size(518, 258);
+            this.txtComentario.TabIndex = 16;
+            this.txtComentario.Text = "txtComentario";
+            // 
+            // Label7
+            // 
+            this.Label7.Location = new System.Drawing.Point(12, 168);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(100, 20);
+            this.Label7.TabIndex = 15;
+            this.Label7.Text = "Comentario:";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnNuevo.Location = new System.Drawing.Point(566, 20);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 6;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnActualizar.Location = new System.Drawing.Point(482, 20);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 5;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // txtFechaAlta
+            // 
+            this.txtFechaAlta.Location = new System.Drawing.Point(120, 136);
+            this.txtFechaAlta.Name = "txtFechaAlta";
+            this.txtFechaAlta.Size = new System.Drawing.Size(140, 20);
+            this.txtFechaAlta.TabIndex = 14;
+            this.txtFechaAlta.Text = "txtFechaAlta";
+            // 
+            // Label6
+            // 
+            this.Label6.Location = new System.Drawing.Point(12, 140);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(100, 20);
+            this.Label6.TabIndex = 13;
+            this.Label6.Text = "Fecha alta:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEmail.Location = new System.Drawing.Point(120, 108);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(518, 20);
+            this.txtEmail.TabIndex = 12;
+            this.txtEmail.Text = "txtEmail";
+            // 
+            // Label5
+            // 
+            this.Label5.Location = new System.Drawing.Point(12, 112);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(100, 20);
+            this.Label5.TabIndex = 11;
+            this.Label5.Text = "e-mail:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNombre.Location = new System.Drawing.Point(120, 80);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(518, 20);
+            this.txtNombre.TabIndex = 10;
+            this.txtNombre.Text = "txtNombre";
+            // 
+            // Label4
+            // 
+            this.Label4.Location = new System.Drawing.Point(12, 84);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(100, 20);
+            this.Label4.TabIndex = 9;
+            this.Label4.Text = "Nombre:";
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.SystemColors.Control;
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(120, 52);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(60, 20);
+            this.txtID.TabIndex = 8;
+            this.txtID.Text = "txtID";
+            // 
+            // Label3
+            // 
+            this.Label3.Location = new System.Drawing.Point(12, 56);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(100, 20);
+            this.Label3.TabIndex = 7;
+            this.Label3.Text = "ID:";
+            // 
+            // Label2
+            // 
+            this.Label2.Location = new System.Drawing.Point(12, 24);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(128, 23);
+            this.Label2.TabIndex = 0;
+            this.Label2.Text = "Botones de movimiento:";
+            // 
+            // btnLast
+            // 
+            this.btnLast.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnLast.Location = new System.Drawing.Point(244, 20);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(28, 23);
+            this.btnLast.TabIndex = 4;
+            this.btnLast.Text = ">|";
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnNext.Location = new System.Drawing.Point(212, 20);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(28, 23);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = ">";
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnPrev.Location = new System.Drawing.Point(180, 20);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(28, 23);
+            this.btnPrev.TabIndex = 2;
+            this.btnPrev.Text = "<";
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnFirst.Location = new System.Drawing.Point(148, 20);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(28, 23);
+            this.btnFirst.TabIndex = 1;
+            this.btnFirst.Text = "|<";
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 610);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.btnExaminar);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button btnExaminar;
+        internal System.Windows.Forms.TextBox txtBase;
+        internal System.Windows.Forms.Button btnConectar;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.TextBox txtComentario;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.Button btnNuevo;
+        internal System.Windows.Forms.Button btnActualizar;
+        internal System.Windows.Forms.TextBox txtFechaAlta;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.TextBox txtEmail;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.TextBox txtNombre;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.TextBox txtID;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Button btnLast;
+        internal System.Windows.Forms.Button btnNext;
+        internal System.Windows.Forms.Button btnPrev;
+        internal System.Windows.Forms.Button btnFirst;
+    }
+}
+
---
+++ AccesoDatosAccess/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace AccesoDatosAccess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // Las variables que usaremos en este ejemplo
+        DataTable dt;
+        SqlDataAdapter da;
+        int fila;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Limpiar los controles del GroupBox y
+            // deshabilitarlos hasta que se conecte a la base de datos
+            //
+            foreach (Control c in this.GroupBox1.Controls)
+            {
+                // Limpiar los textbox
+                if (c is TextBox)
+                {
+                    c.Text = "";
+                }
+                // Deshabilitarlos
+                c.Enabled = false;
+            }
+            this.GroupBox1.Enabled = false;
+            this.GroupBox1.Text = "Debes conectar antes de usar los datos";
+            //
+            // El nombre de la base de datos:
+            // (poner el path real de la base de datos de prueba)
+            this.txtBase.Text = "db2000.mdb";
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.IO.File.Exists(txtBase.Text) == false)
+                {
+                    MessageBox.Show("No existe la base de datos indicada");
+                    txtBase.Focus();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message + "\nSeguramente porque no existe la base de datos indicada");
+                txtBase.Focus();
+                return;
+            }
+            //
+            // La cadena de conexión
+            string sCnn = "Data Source=" + txtBase.Text + "; Initial Catalog=pruebaDB; User ID=SA; Password=oretania";
+            // La cadena de selección
+            string sSel = "SELECT * FROM Prueba ORDER BY ID";
+            // Para traer solo los registros entre dos fechas
+            // sSel = "SELECT * FROM Prueba WHERE (FechaAlta >= #2006/01/05# AND FechaAlta <= #2006/01/06#)";
+
+
+            // Comprobar si hay algún error
+            try
+            {
+                // Crear un nuevo objeto del tipo DataAdapter
+                //Dim cnn As New OleDbConnection(sCnn)
+                da = new SqlDataAdapter(sSel, sCnn);
+                // Crear los comandos de insertar, actualizar y eliminar
+                SqlConnection cnx = new SqlConnection(sCnn);
+                // Como hay campos con caracteres especiales,
+                // al usarlos incluirlos entre corchetes.
+                cb.QuotePrefix = "[";
+                cb.QuoteSuffix = "]";
+                // Asignar los comandos al DataAdapter
+                // (se supone que lo hace automáticamente, pero...)
+                da.UpdateCommand = cb.GetUpdateCommand();
+                da.InsertCommand = cb.GetInsertCommand();
+                da.DeleteCommand = cb.GetDeleteCommand();
+                //
+                // Esta base de datos usa el ID con valores automáticos
+                da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+                //
+                dt = new DataTable();
+                // Llenar la tabla con los datos indicados
+                da.Fill(dt);
+                //
+                // Habilitar los controles
+                foreach (Control c in this.GroupBox1.Controls)
+                {
+                    c.Enabled = true;
+                }
+                this.GroupBox1.Enabled = true;
+                this.GroupBox1.Text = "Conexión realizada";
+
+                // Y mostrar el primer registro
+                if (dt.Rows.Count > 0)
+                {
+                    btnFirst_Click(null, null);
+                }
+                else
+                {
+                    fila = -1;
+                    btnActualizar.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR al conectar o recuperar los datos:\n" + ex.Message,
+                                "Conectar con la base",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            // Crear un nuevo registro
+            DataRow dr = dt.NewRow();
+            // Asignar los datos de los textbox a la fila
+            asignarDatos(dr);
+            // Añadir la nueva fila a la tabla
+            dt.Rows.Add(dr);
+            // Guardar físicamente los datos en la base
+            try
+            {
+                da.Update(dt);
+                dt.AcceptChanges();
+                // Si es el primer registro de la base,
+                // volver a leer los datos para actualizar los IDs
+                if (Convert.ToInt32("0" + dr["ID"].ToString()) == 0)
+                {
+                    dt = new DataTable();
+                    da.Fill(dt);
+                }
+                // Posicionarlo en la última fila
+                btnLast_Click(null, null);
+            }
+            catch (DBConcurrencyException ex)
+            {
+                MessageBox.Show("Error de concurrencia:\n" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+         
+            }
+        }
+
+        private void asignarDatos(DataRow dr)
+        {
+            // Usar los datos que hay en los textbox
+            dr["Nombre"] = txtNombre.Text;
+            dr["e-mail"] = txtEmail.Text;
+            dr["FechaAlta"] = txtFechaAlta.Text;
+            dr["Comentario"] = txtComentario.Text;
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            // Actualizar los datos en la fila actual
+            if (fila < 0 || fila > dt.Rows.Count - 1) return;
+
+            DataRow dr = dt.Rows[fila];
+            // Asignar los datos de los textbox a la fila
+            asignarDatos(dr);
+            // Guardar físicamente los datos en la base
+            try
+            {
+                da.Update(dt);
+                dt.AcceptChanges();
+            }
+            catch (DBConcurrencyException ex)
+            {
+                MessageBox.Show("Error de concurrencia:\n" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void mostrarDatos(int f)
+        {
+            int uf = dt.Rows.Count - 1;
+            if (f < 0 || uf < 0) return;
+            //
+            DataRow dr = dt.Rows[f];
+            txtID.Text = dr["ID"].ToString();
+            txtNombre.Text = dr["Nombre"].ToString();
+            txtEmail.Text = dr["e-mail"].ToString();
+            txtFechaAlta.Text = dr["FechaAlta"].ToString();
+            txtComentario.Text = dr["Comentario"].ToString();
+            //
+            btnActualizar.Enabled = true;
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            // Posicionarse en la primera fila
+            fila = 0;
+            // Mostrar los datos de la fila indicada
+            mostrarDatos(fila);
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            // Posicionarse en la fila anterior
+            fila = fila - 1;
+            if (fila < 0) fila = 0;
+            // Mostrar los datos de la fila indicada
+            mostrarDatos(fila);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            // Posicionarse en la fila siguiente
+            int uf = dt.Rows.Count - 1;
+            fila = fila + 1;
+            if (fila > uf) fila = uf;
+            // Mostrar los datos de la fila indicada
+            mostrarDatos(fila);
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            // Posicionarse en la última fila
+            fila = dt.Rows.Count - 1;
+            // Mostrar los datos de la fila indicada
+            mostrarDatos(fila);
+        }
+    }
+}
